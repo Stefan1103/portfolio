4f69bdde3b665ade9cfdca29dbafb1ac2d61ea49
Stefan1103 <stefan.stevanovski11@gmail.com>
2022-10-12 14:23:26 +0200

    fixed the text of the description of the projects.

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,43 +1,41 @@
-import React from 'react';
-import { useParams } from 'react-router';
-import { sliderProjects } from '../../data';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router";
+import { sliderProjects } from "../../data";
+import { Link } from "react-router-dom";
 
 const Description = () => {
-	const { clickedId } = useParams();
-	const filteredProjects = sliderProjects.filter((project) => {
-		console.log(project.id);
+  const { clickedId } = useParams();
+  const filteredProjects = sliderProjects.filter((project) => {
+    console.log(project.id);
 
-		return project.id === parseInt(clickedId);
-	});
-	console.log(filteredProjects);
-	const { icon, title, id, desc, webLink, gitLink, img } = filteredProjects[0];
-	console.log(img);
-	return (
-		<div className="containerDesc">
-			<h2>{title}</h2>
-			<div className="innerContainer">
-				<div className="left">
-					<img src={`/${img}`} alt="#" />
-				</div>
-				<div className="right">
-					<p>{desc}</p>
-				</div>
-			</div>
+    return project.id === parseInt(clickedId);
+  });
+  const { icon, title, id, desc, webLink, gitLink, img } = filteredProjects[0];
+  return (
+    <div className="containerDesc">
+      <h2>{title}</h2>
+      <div className="innerContainer">
+        <div className="left">
+          <img src={`/${img}`} alt="#" />
+        </div>
+        <div className="right">
+          <p>{desc}</p>
+        </div>
+      </div>
 
-			<div className="btn-container">
-				<Link className="btn-back" to="/">
-					Back
-				</Link>
-				<a className="btn-main" href={webLink} target="_blank">
-					Got to website
-				</a>
-				<a className="btn-main" href={gitLink} target="_blank">
-					Go to source code
-				</a>
-			</div>
-		</div>
-	);
+      <div className="btn-container">
+        <Link className="btn-back" to="/">
+          Back
+        </Link>
+        <a className="btn-main" href={webLink} target="_blank">
+          Got to website
+        </a>
+        <a className="btn-main" href={gitLink} target="_blank">
+          Go to source code
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Description;
--- src/data.js
+++ src/data.js
@@ -1,119 +1,118 @@
 export const sidebarItems = [
-	{
-		id: 1,
-		name: 'Home',
-		url: '#intro',
-	},
-	{
-		id: 2,
-		name: 'Portfolio',
-		url: '#portfolio',
-	},
-	{
-		id: 3,
-		name: 'Projects',
-		url: '#works',
-	},
-	{
-		id: 5,
-		name: 'Contact',
-		url: '#contact',
-	},
+  {
+    id: 1,
+    name: "Home",
+    url: "#intro",
+  },
+  {
+    id: 2,
+    name: "Portfolio",
+    url: "#portfolio",
+  },
+  {
+    id: 3,
+    name: "Projects",
+    url: "#works",
+  },
+  {
+    id: 5,
+    name: "Contact",
+    url: "#contact",
+  },
 ];
 
 export const portfolioListItems = [
-	{
-		id: 1,
-		title: 'Webb App',
-	},
-	{
-		id: 2,
-		title: 'Mobile App',
-	},
-	{
-		id: 3,
-		title: 'Design',
-	},
+  {
+    id: 1,
+    title: "Webb App",
+  },
+  {
+    id: 2,
+    title: "Mobile App",
+  },
+  {
+    id: 3,
+    title: "Design",
+  },
 ];
 
 export const webbAppPortfolio = [
-	{
-		id: 1,
-		title: 'Movies App',
-		img: 'assets/movies_roll_tape.png',
-		webLink: 'https://compassionate-brown-bb6b8a.netlify.app/',
-	},
-	{
-		id: 2,
-		title: 'Flea market',
-		img: 'assets/Flea_market.jpg',
-		webLink: 'https://elastic-shirley-36b743.netlify.app/',
-	},
-	{
-		id: 3,
-		title: 'Portfolio',
-		img: 'assets/portfolio.jpg',
-		webLink: 'https://github.com/Stefan1103/portfolio',
-	},
+  {
+    id: 1,
+    title: "Movies App",
+    img: "assets/movies_roll_tape.png",
+    webLink: "https://compassionate-brown-bb6b8a.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Flea market",
+    img: "assets/Flea_market.jpg",
+    webLink: "https://elastic-shirley-36b743.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    img: "assets/portfolio.jpg",
+    webLink: "https://github.com/Stefan1103/portfolio",
+  },
 ];
 export const designPortfolio = [
-	{
-		id: 1,
-		title: 'Movies App',
-		img: 'assets/movies_roll_tape.png',
-		webLink: 'https://compassionate-brown-bb6b8a.netlify.app/',
-	},
-	{
-		id: 2,
-		title: 'Flea market',
-		img: 'assets/Flea_market.jpg',
-		webLink: 'https://elastic-shirley-36b743.netlify.app/',
-	},
-	{
-		id: 3,
-		title: 'Portfolio',
-		img: 'assets/portfolio.jpg',
-		webLink: 'https://github.com/Stefan1103/portfolio',
-	},
+  {
+    id: 1,
+    title: "Movies App",
+    img: "assets/movies_roll_tape.png",
+    webLink: "https://compassionate-brown-bb6b8a.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Flea market",
+    img: "assets/Flea_market.jpg",
+    webLink: "https://elastic-shirley-36b743.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Portfolio",
+    img: "assets/portfolio.jpg",
+    webLink: "https://github.com/Stefan1103/portfolio",
+  },
 ];
 
 export const mobileAppPortfolio = [];
 
 export const sliderProjects = [
-	{
-		id: 1,
-		icon: 'assets/globe.png',
-		title: 'Movies App',
-		shortDesc:
-			'Web application that enables searching for all movies and inofrmation details about the movie, in order to achive this the app uses the open source OMDB API. Technologies that are used for this app are : React,Html,Scss.',
-		desc:
-			'Web application that enables searching for all movies. Also this app shows details and information about the movies like ratings and cast for the movie, in order to achive that the app uses the open source OMDB API. Technologies used to make this app are React(for the state managment are used React-hooks), HTML, SCSS and Bootstrap. The app can be used by utilizing the search bar to find a movie(s) in the Api which are showcased by a carousel and cards, also you can navigate to the details for the selected movie and see the information from the OMDB Api about the movie',
-		img: 'assets/movies_roll_tape.png',
-		webLink: 'https://613c7cc0e06be00007fab640--compassionate-brown-bb6b8a.netlify.app/',
-		gitLink: 'https://github.com/Stefan1103/movies-app',
-	},
-	{
-		id: 2,
-		icon: 'assets/globe.png',
-		title: 'Flea market',
-		shortDesc:
-			'This is a Web application for a retail online store/market. This app among others technologies is based on the MERN(Mongo,Express,React,Node) stack, and for the state menagment of the app is utilized "Redux". ',
-		desc:
-			"This is a Web application for a retail online store/market. The app's Landing page showcases the stores featured itemas that are on a sale in a carousel also you can see other items that are not on sale in the cards below. For navigation and selection for a specific item(s) from the store you can use either the search bar or the categories drop down button. You can select items for buying by clicking the button with the cart+ icon or you can see details about the specific item buy clicking the Learn more button which will take you to the details page for the selected item. By clicking the button for adding items to your cart you can see the number of items update in the right corner of the navbar. To access your cart you can simply click on the right corner of the navbar cart icon, it will take you to the items in your cart page. In this page you can see your items maniuplate with the cart how ever you want and when you are satisfied you can go to the checkout page. Here you also have an option if you are not satisfied with the selection to go back to the cart if evrything looks good you can proceed to pay for the items. For this app thechnologies that are used are : the full stack MERN(Mongo,Express,React,Node), Scss,HTML,Bootstrap. For the state managment of this app the usefull tool Redux is used. This project's client side is hosted on netfly and the server side on heroku.",
-		img: 'assets/Flea_market.jpg',
-		webLink: 'https://elastic-shirley-36b743.netlify.app/',
-		gitLink: 'https://github.com/Stefan1103/eCommerce-app',
-	},
-	{
-		id: 3,
-		icon: 'assets/mobile.png',
-		title: 'Portfolio',
-		shortDesc: 'This website project is the actual portfolio site. This project Mainly uses React and its packages',
-		desc:
-			'This website project is the actual portfolio site. Technologies used are: React, Html and scss. And for the contact form is used emailJS. this project is hosted on netlify.',
-		img: 'assets/portfolio.jpg',
-		gitLink: 'https://github.com/Stefan1103/portfolio',
-	},
+  {
+    id: 1,
+    icon: "assets/globe.png",
+    title: "Movies App",
+    shortDesc:
+      "Web application that enables searching for all movies and inofrmation details about the movie, in order to achive this the app uses the open source OMDB API. Technologies that are used for this app are : React,Html,Scss.",
+    desc: "Web application that enables searching for all movies. Also this app shows details and information about the movies, like ratings and cast for the movie, in order to achive this the app uses an open source API (OMDB-API). The technologies that are used to make this web app are: React, HTML, SCSS and Bootstrap. The app can be used by utilizing the search bar which searches for specific movie or movies by the name of the movie, example of a search: Batman.The webb app will provide the search results from the Api which are showcased by a carousel and cards, also you can navigate to the details for the selected movie and see the information from the OMDB Api about the movie and its details.",
+    img: "assets/movies_roll_tape.png",
+    webLink:
+      "https://613c7cc0e06be00007fab640--compassionate-brown-bb6b8a.netlify.app/",
+    gitLink: "https://github.com/Stefan1103/movies-app",
+  },
+  {
+    id: 2,
+    icon: "assets/globe.png",
+    title: "Flea market",
+    shortDesc:
+      'This is a Web application for a retail online store/market. This app among others technologies is based on the MERN(Mongo,Express,React,Node) stack, and for the state menagment of the app is utilized a state manager: "Redux". ',
+    desc: "This is a Web application for a retail online store/market. The app's Landing page showcases the stores featured items that are on a sale in a carousel, also you can see other items that are not on sale in the cards below. For navigation and selection for a specific item(s) from the store you can use either the search bar and search for the name of the desired item or you can see the items by categories from the drop down button. You can select items for buying by clicking the button with the cart+ icon or you can see details about the specific item by clicking the Learn more button which will take you to the details page for the selected item. By clicking the button for adding items to your cart you can see the number of items update in the right corner of the navbar. To access your cart you can simply click on the right corner of the navbar cart icon, it will take you to the items in your cart page. In this page you can see your items maniuplate with the cart how ever you want and when you are satisfied you can go to the checkout page. Here you also have an option if you are not satisfied with the selection to go back to the cart and double check your purchase. On the other hand if evrything looks good you can proceed to pay for the items. For this app thechnologies that are used are : the stack MERN(Mongo,Express,React,Node), Scss,HTML,Bootstrap. For the state managment of this app is used REDUX the state manager tool. This project's client side is hosted on netfly and the server side on heroku.",
+    img: "assets/Flea_market.jpg",
+    webLink: "https://elastic-shirley-36b743.netlify.app/",
+    gitLink: "https://github.com/Stefan1103/eCommerce-app",
+  },
+  {
+    id: 3,
+    icon: "assets/mobile.png",
+    title: "Portfolio",
+    shortDesc:
+      "This website project is the actual portfolio site. This project Mainly uses React and its packages",
+    desc: "This website project is the actual portfolio site. Technologies used are: React, Html and scss. And for the contact form is used emailJS. this project is hosted on netlify.",
+    img: "assets/portfolio.jpg",
+    gitLink: "https://github.com/Stefan1103/portfolio",
+  },
 ];
 
-export const underContst = 'assets/under_construction.png';
+export const underContst = "assets/under_construction.png";
